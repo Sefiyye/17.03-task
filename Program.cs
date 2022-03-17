6b22693dc2ea62ef7c4679a42576138f951c9769
Sefiyye <tu7l1lxat@code.edu.az>
2022-03-17 12:12:12 +0400

    17 mart task

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("okay", "sdfeeg");
+            Console.WriteLine(user.Username);
+            Console.WriteLine(user.Password);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    internal class User
+    {
+        private string _username;
+        private string _password;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                if (value.Length > 6)
+                {
+                    _username = value;
+                }
+                else
+                {
+                    Console.WriteLine("6-dan boyuk olmalidir");
+                }
+            }
+        }
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                if( _password.Length>0)
+                {
+                    _password = value;
+                }
+                else
+                {
+                    Console.WriteLine("minimum 1 herf daxil etmelisiz");
+                }
+            }
+        }
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+
+
+        }
+    }
+}
